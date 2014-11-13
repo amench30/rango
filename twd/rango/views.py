1f65dc5e7963e68c575b5199649dd26c63b95301
mencher@gmail.com <Yankees10>
2014-11-12 20:02:30 -0800

    rango project

--- twd/rango/views.py
+++ twd/rango/views.py
@@ -8,4 +8,6 @@ def index(request):
     context_dict = {}
     return render_to_response("rango/index.html", context_dict, context)
 def about(request):
-    return HttpResponse('Rango says: Here is the about page<a href="/rango/">Click Me</a>')
+    context = RequestContext(request)
+    context_dict = {}
+    return render_to_response("rango/about.html", context_dict, context)
